afbc95bfb2f953d5191490ce6ef29972235c67a2
Onyango Mary <adongorowa@gmail.com>
2026-04-21 22:40:43 +0300

    sql table realtions at initial_final update

--- main.py
+++ main.py
@@ -11,37 +11,84 @@ pd.read_sql("""SELECT * FROM sqlite_master""", conn)
 
 # STEP 1
 # Replace None with your code
-df_boston = None
+df_boston = """
+    SELECT firstName, lastName, jobTitle
+    FROM employees
+    INNER JOIN offices 
+        USING(officeCode)
+    WHERE city = "Boston";
+"""
 
 # STEP 2
 # Replace None with your code
-df_zero_emp = None
+df_zero_emp = """
+    SELECT officeCode, COUNT(officeCode) AS total_per_office
+    FROM offices 
+    LEFT JOIN employees
+        USING(officeCode)
+    GROUP BY officeCode
+    HAVING (employees.firstName, employees.lastName) IS NULL;
+"""
 
 # STEP 3
 # Replace None with your code
-df_employee = None
+df_employee = """
+    SELECT employees.firstName, employees.lastName, offices.city, offices.state
+    FROM employees
+    LEFT JOIN offices
+        USING(officeCode)
+    ORDER BY employees.firstName, employees.lastName;
+"""
 
 # STEP 4
 # Replace None with your code
-df_contacts = None
+df_contacts = """
+    SELECT contactFirstName, contactLastName, phone, salesRepEmployeeNumber
+    FROM customers
+    LEFT JOIN orders
+        USING(customerNumber)
+    WHERE customerNumber IS NULL
+    ORDER BY contactLastName;
+"""
 
 # STEP 5
 # Replace None with your code
-df_payment = None
+df_payment = """
+    SELECT customers.contactFirstName, customers.contactLastName, CAST(payments.amount AS INTEGER), payments.paymentDate
+    FROM customers
+    LEFT JOIN payments
+        USING(customerNumber)
+    ORDER BY payments.amount DESC;
+"""
 
 # STEP 6
 # Replace None with your code
-df_credit = None
+df_credit = """
+    SELECT employees.employeeNumber, employees.firstName, employees.lastName, SUM(customers.customerNumber) AS numberOfCustomers
+    FROM employees
+    INNER JOIN customers
+        USING(customers.contactLastName AND customers.contactFirstName)
+    WHERE customers.creditLimit > 90000
+    GROUP BY no_of_customers DESC
+    LIMIT 4;
+"""
 
 # STEP 7
 # Replace None with your code
-df_product_sold = None
+df_product_sold = """
+    SELECT products.productName, COUNT(orderdetails.quantityOrdered) AS numorders, SUM(orderdetails.quantityOrdered) AS totalunits
+    FROM products
+    LEFT JOIN orderdetails
+        USING(productCode)
+    GROUP BY productCode
+    ORDER BY totalunits DESC;
+"""
 
 # STEP 8
 # Replace None with your code
 df_total_customers = None
 
-# STEP 9
+# STEP 9  
 # Replace None with your code
 df_customers = None
 
